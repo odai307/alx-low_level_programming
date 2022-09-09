b249dae3cd3b78f76010a9b60966b893407fe414
odai307 <gabrielafotey@gmail.com>
2022-09-09 03:51:11 +0000

    and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,9 @@
 /**
  * main - prints exancty 'and that piece of art is useful'
  * -Dora Korpar which is twenty fifteen
- *  Return - Always 0 as it is useful
+ *  Return: 0
  */
 int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}
