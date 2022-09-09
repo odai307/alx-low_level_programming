ebda563921ac75223bdc9bf50b029f494272e74c
odai307 <gabrielafotey@gmail.com>
2022-09-09 01:46:28 +0000

    C program that prints followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+\**
+*main- Entry point of code*
+*Return: it should always be zero
+*\
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+return (0);
 }
