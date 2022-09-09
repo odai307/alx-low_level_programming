0c9043976785d4ca1a47240e4712be0217ae7186
odai307 <gabrielafotey@gmail.com>
2022-09-09 02:51:17 +0000

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include<unistd.h>
 /*
- * main - prints exactly "and that piece of art is usefil"
- * 
+ * main: prints exactly "and that piece of art is usefil"* 
  * Dora Korpar, 2015-10-19
  * followed by a new line, to the standard error
- * 
  * Return: It should always be zero
  */
 int main(void)
