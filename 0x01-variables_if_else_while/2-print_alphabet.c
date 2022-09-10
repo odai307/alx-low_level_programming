df454ef18b867c1ad123b2ba3b75379c3b851019
odai307 <gabrielafotey@gmail.com>
2022-09-10 00:56:26 +0000

    use putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return: Always zero 0 (sucess)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
