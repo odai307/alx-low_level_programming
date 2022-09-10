eb9b7155bf008e34977439cd19653d41208298ec
odai307 <gabrielafotey@gmail.com>
2022-09-10 02:30:46 +0000

    check if positive zero or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-	printf("%d id positive\n", n);
+	printf("%d is positive\n", n);
 }
-else if (n ==0)
+else if (n == 0)
 {
 	printf("%d is zero\n", n);
 }
