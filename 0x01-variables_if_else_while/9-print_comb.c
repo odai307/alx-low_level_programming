b932c8e096590bd30c5d16fa7626d218148f6520
odai307 <gabrielafotey@gmail.com>
2022-09-10 01:44:46 +0000

    print all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - prints numbers and letters with commas and spaces
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+for (i = 48; i < 58; i++)
+{
+	putchar(i);
+	if (i !=57)
+	{
+		putchar(',');
+		putchar(',');
+	}
+}
+putchar('\n');
+return (0);
+}
