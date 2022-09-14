16275565f716d7b9e54cc351c871471e678efe89
odai307 <gabrielafotey@gmail.com>
2022-09-14 01:50:19 +0000

    4-isalphs

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,17 @@
-#include "main
+#include "main.h"
+/**
+ * int_isalpha - checks alphabetic character
+ *
+ * Return: o
+ */
+int _isalpha(int c)
+{
+	if ( c > 'a' || c > 'A')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
