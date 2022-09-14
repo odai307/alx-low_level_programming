96ea86ff3a30001924fc3d3c46e1732e205d7e17
odai307 <gabrielafotey@gmail.com>
2022-09-14 02:15:21 +0000

    absolute value;

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _abs(int) - prints the absolute value of an integer
+ *@c: is the int that will use for the argument of the function
+ * Return: Always 0
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+}
