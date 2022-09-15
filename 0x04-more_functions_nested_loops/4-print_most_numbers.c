57dacb0e14c8e5b9d2307ba203e921119b074455
odai307 <gabrielafotey@gmail.com>
2022-09-15 21:24:29 +0000

    print numbers except some

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print most numbers
+ *
+ * Return: always 0
+ */
+void print_most_numbers(void)
+{
+	char n;
+	
+	for (n = 48; n <=57; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
+
