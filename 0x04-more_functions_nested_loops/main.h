7ad8e6a7855588984cd86b69adbef3fadc342e75
odai307 <gabrielafotey@gmail.com>
2022-09-15 21:12:22 +0000

    print number from 0 to 9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,11 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,5 +17,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(char);
 
-#endif
+#endif /* MAIN_H */
+
