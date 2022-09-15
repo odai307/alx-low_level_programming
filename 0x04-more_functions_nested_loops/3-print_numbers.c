c2e83db95e5a49945b37b417e875f65644f227d2
odai307 <gabrielafotey@gmail.com>
2022-09-15 20:48:37 +0000

    print numbers from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers: prints numbers 
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+}
