21734921931c057216c0ff4f236791f285d94bd9
odai307 <gabrielafotey@gmail.com>
2022-09-16 00:09:08 +0000

    print from zero to nine

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,13 @@
 void print_most_numbers(void)
 {
 	char n;
-	
-	for (n = 48; n <=57; n++)
+
+	for (n = 48; n <= 57; n++)
 	{
-		if (n != 50 && n != 52)
+		if (n != 50 || n != 52)
 		{
 			_putchar(n);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
-
