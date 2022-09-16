ed75dd440c53e61d3bf4a4038e7abe36b0642d4f
odai307 <gabrielafotey@gmail.com>
2022-09-16 00:50:40 +0000

    prints diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
- * print_diagonal - prints a diagonal
+ * ptint_diagonal - prints a diagonal
+ *
  * @n: print \n when n is 0 or less
  * Return: 0
  */
-void print_diagonal(int n)
+void ptint_diagonal(int n)
 {
 	int len, space;
-	
+
 	if (n > 0)
 	{
-		for (len = o; len < n; len++)
+		for (len = 0; len < n; len++)
 		{
 			for (space = 0; space < len; space++)
 			{
 				_putchar(' ');
 			}
-			putchar('\\');
+			_putchar('\\');
 			if (len == (n - 1))
 			{
 				continue;
@@ -24,5 +25,5 @@ void print_diagonal(int n)
 			_putchar('\n');
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
