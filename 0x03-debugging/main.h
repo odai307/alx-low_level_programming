84d52ac6cc1526af53e372c8cd3af1b8d919aa05
odai307 <gabrielafotey@gmail.com>
2022-09-16 18:48:17 +0000

    largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_numbers(int, int, int);
+int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_days(int, int);
 
