f769c1034cd5df7d70d63829ecab965767c5ac67
odai307 <gabrielafotey@gmail.com>
2022-09-16 20:05:43 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,34 +5,35 @@
  */
 int main(void)
 {
-	int num = 0;
+	int n;
 
-	while (num <= 100)
+	for (n = 1; n <= 100; n++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if (num % 3 == 0)
+		else if (n % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (num % 5 == 0)
+		else if (n % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
-			if (num == 100)
+			if (n == 100)
 			{
-				printf("%d", num);
+				printf("FizzBuzz");
 			}
 			else
 			{
-				printf("%d ", num);
+				printf("%d ", n);
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
-	}
 }
+
