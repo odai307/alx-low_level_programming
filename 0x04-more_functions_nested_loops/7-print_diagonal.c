faaf6d83a137eecaaf1590af56cfe6cfecce7032
odai307 <gabrielafotey@gmail.com>
2022-09-16 00:43:18 +0000

    draw a diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,3 +1,28 @@
 #include "main.h"
 /**
- *
+ * print_diagonal - prints a diagonal
+ * @n: print \n when n is 0 or less
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int len, space;
+	
+	if (n > 0)
+	{
+		for (len = o; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				_putchar(' ');
+			}
+			putchar('\\');
+			if (len == (n - 1))
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	putchar('\n');
+}
