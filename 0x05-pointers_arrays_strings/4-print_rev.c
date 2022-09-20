d72b26763d99f583a0bacc4216abe6c31a4e890c
odai307 <gabrielafotey@gmail.com>
2022-09-20 00:04:05 +0000

    print in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: parameter
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
